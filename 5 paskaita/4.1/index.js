7cff4f0771dda0627e7e436702d88e671354075c
jurgar <102683510+jurgar@users.noreply.github.com>
2022-08-03 22:04:37 +0300

    server

---
+++ 5 paskaita/4.1/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+const cars = {
+  bmw: ["i3", "i8", "1 series", "3 series", "5 series"],
+  mb: ["A class", "C class", "E class", "S class"],
+  vw: ["Golf", "Arteon", "UP"],
+};
+
+app.get("/cars/:model", (req, res) => {
+  console.log(req.params.model);
+  res.send("cars/bmw");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server is running on https://localhost:${PORT}`)
+);
